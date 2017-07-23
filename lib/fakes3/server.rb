ba528aa89ce996f3f2872c8002014745486efa8c
Claudio Poli <masterkain@gmail.com>
2017-07-23 07:19:32 +0200

    Update server.rb

--- lib/fakes3/server.rb
+++ lib/fakes3/server.rb
@@ -54,6 +54,8 @@ module FakeS3
       @hostname = hostname
       @port = server.config[:Port]
       @root_hostnames = [hostname,'localhost','s3.amazonaws.com','s3.localhost']
+      escaped_hostnames = @root_hostnames.map { |host| Regexp.escape(host) }
+      @bucket_host_regex = Regexp.new('^(.*?)\.(?:%s)$' % escaped_hostnames.join('|'))
     end
 
     def validate_request(request)
@@ -479,7 +481,7 @@ module FakeS3
       s_req.is_path_style = true
 
       if !@root_hostnames.include?(host) && !(IPAddr.new(host) rescue nil)
-        s_req.bucket = host.split(".")[0]
+        s_req.bucket = @bucket_host_regex.match(host)[1]
         s_req.is_path_style = false
       end
 
